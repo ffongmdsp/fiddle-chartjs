b476d23b2b84ae3e467f9d5119a905eec7b749b0
Frankie Fong <frankie@minidsp.com>
2022-01-24 17:38:51 +0800

    Skip x value of 0. log(0) is undefined. ChartJs would skip and start at 100 instead when x-axis is logarithmic scale. Cannot find any known good solution.

--- tooltip_linechart.js
+++ tooltip_linechart.js
@@ -1,12 +1,19 @@
+// https://github.com/chartjs/Chart.js/issues/2966
+// https://github.com/chartjs/Chart.js/issues/9629
+// Avoid 0 x-value because log(0) fails - the
+// x-axis logarithmic scale then starts at 100.
 // Sine Wave from 0 to 20kHz
 let sineWavePts = [];
-for (let f = 0; f < 2000; f++) {
+//let f = 0.1;
+//sineWavePts.push({ 'x': f, 'y': Math.sin(f * Math.PI / 180)});
+for (let f = 1; f < 2000; f++) {
   sineWavePts.push({ 'x': f, 'y': Math.sin(f * Math.PI / 180)});
   //wavePts.push({ 'x': f, 'y': f * 3 });
 }
-
 let cosineWavePts = [];
-for (let f = 0; f < 2000; f++) {
+//f = 0.1;
+//cosineWavePts.push({ 'x': f, 'y': Math.cos(f * Math.PI / 180)});
+for (let f = 1; f < 2000; f++) {
   cosineWavePts.push({ 'x': f, 'y': Math.cos(f * Math.PI / 180)});
 }
 
@@ -51,7 +58,8 @@ const config = {
         title: {
           display: true,
           text: 'Frequency (Hz)'
-        }
+        },
+        beginAtZero: true
       },
       y: {
         type: 'linear',
@@ -60,7 +68,8 @@ const config = {
         title: {
           display: true,
           text: 'Gain (dB)'
-        }
+        },
+        beginAtZero: true
       }
     },
     interaction: {
